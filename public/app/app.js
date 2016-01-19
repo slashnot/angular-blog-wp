c955ab194ef24870b59a36f2546acba10d1563c0
Ramkumar <slashnot84@gmail.com>
2016-01-19 17:42:41 +0530

    Added Basic Routing using UI.Router

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,25 @@
  * MAIN ANGULAR APP MODULE
  */
 
-angular.module('blogApp',['ngMaterial'])
-.controller('BlogCtrl',[function(){
-    
-}]);
+angular.module("blogApp", ["ngMaterial", "oc.lazyLoad", "ui.router"])
+
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        
+        $urlRouterProvider.otherwise("/");
+
+        // Now set up the states
+        $stateProvider
+            .state('home', {
+                url: "/",
+                templateUrl: "app/pages/home/home.tpl.html"
+            })
+            .state('blog', {
+                url: "/blog",
+                templateUrl: "app/pages/blog/blog.tpl.html"
+            });
+        //$locationProvider.html5Mode(true);
+    })
+
+    .controller('BlogCtrl', [function () {
+
+    }]);
---
+++ public/app/pages/blog/blog.js
@@ -0,0 +1,22 @@
+﻿
+(function () {
+    'use strict';
+
+    angular.module('app.blog', [
+        // Angular modules 
+        'oc.lazyload',
+        'ui.router'
+    ])
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+
+        $urlRouterProvider.otherwise("/");
+
+        // Now set up the states
+        $stateProvider
+            .state('blog.archive', {
+                url: "/archive",
+                templateUrl: "app/pages/blog/archive.tpl.html"
+            });
+        //$locationProvider.html5Mode(true);
+    })
+})();
