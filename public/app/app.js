321725bb10bebe31ee1dbc5f79ee32b9be36b0a2
Ramkumar <slashnot84@gmail.com>
2016-01-23 20:18:19 +0530

    Some tests and bug fixes

--- public/app/app.js
+++ public/app/app.js
@@ -3,17 +3,21 @@
  * MAIN ANGULAR APP MODULE
  */
 
-angular.module("blogApp", ["afm.boot"])
+angular.module("blogApp", ["afm.boot", "ngResource"])
 
-    
+.factory('Entry', function ($resource) {
+    return $resource('http://127.0.0.1/WORKS/Wp-Angular/wp-json/wp/v2/posts/', {},
+       {
+           get: {
+               method: 'GET',
+               isArray: true
+           }
+       }); // Note the full endpoint address
+})
 
-    //Main Application Controller Logic
-    .controller('BlogCtrl', [function () {
-
-
-    }]);
-angular.element(document).ready(function () {
-    console.log("Ready");
-    angular.module("testApp", []);
-    angular.module("testApp1", ["testApp"]);
-});
+//Main Application Controller Logic
+.controller('BlogCtrl', ["Entry", function (Entry) {
+    var entry = Entry.get( function () {
+        console.log(entry);
+    });
+}]);
